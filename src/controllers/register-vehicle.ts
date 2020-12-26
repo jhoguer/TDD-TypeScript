dc3008ada9b6f1e50fe80c61a5896c7cac3d8a32
Jhon Guerrero <jhoguer@gmail.com>
2020-12-26 18:53:55 -0500

    refactor: add RegisterVehicle

--- src/controllers/register-vehicle.ts
+++ src/controllers/register-vehicle.ts
@@ -2,17 +2,13 @@ import { HttpResponse, HttpRequest } from '../interfaces/http-interface'
 
 export class RegisterVehicle {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (httpRequest.body.name === undefined) {
-      return {
-        statusCode: 400,
-        body: new Error('error in the: name')
-      }
-    }
-
-    if (httpRequest.body.model === undefined) {
-      return {
-        statusCode: 400,
-        body: new Error('error in the: model')
+    const requireProperties = ['name', 'model', 'year', 'color']
+    for (const props of requireProperties) {
+      if (httpRequest.body[props] === undefined) {
+        return {
+          statusCode: 400,
+          body: new Error(`error in the: ${props}`)
+        }
       }
     }
   }
