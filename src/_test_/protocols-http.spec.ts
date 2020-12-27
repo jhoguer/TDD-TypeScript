b7c5602a82c11cd34ca831dbff67c93817c058e7
Jhon Guerrero <jhoguer@gmail.com>
2020-12-27 17:08:41 -0500

    test: add invalidArgument in UrlLogin

---
+++ src/_test_/protocols-http.spec.ts
@@ -0,0 +1,29 @@
+import { InvalidArgument } from '../errors/invalid-arg'
+import { UrlLogin } from '../protocols/protocol-http'
+
+describe('Protocols Http and Querys', () => {
+  test('Url login', () => {
+    const parsedUrl = UrlLogin.parseUrl('http://localhost:3000/login')
+
+    expect(parsedUrl.href).toEqual('http://localhost:3000/login')
+    expect(parsedUrl.port).toBe('3000')
+  })
+
+  test('Response Query', () => {
+    const parsedUrl = UrlLogin.parseUrl('http://localhost:3000/login?user=user&password=password')
+    const expectAuth = {
+      user: 'user',
+      password: 'password'
+    }
+
+    expect(parsedUrl.query).toEqual(expectAuth)
+  })
+
+  test('Invalid Url', () => {
+    function expectError (): void {
+      UrlLogin.parseUrl('')
+    }
+
+    expect(expectError).toThrowError(new InvalidArgument(''))
+  })
+})
