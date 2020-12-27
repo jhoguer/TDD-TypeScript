cfca8dd2cab35e131eb4d5841fbc933cb42179d1
Jhon Guerrero <jhoguer@gmail.com>
2020-12-26 19:19:03 -0500

    refactor: add MissingFormalParameter in the Register vehicle

--- src/controllers/register-vehicle.ts
+++ src/controllers/register-vehicle.ts
@@ -1,3 +1,4 @@
+import { MissingFormalParameter } from '../errors/client-error'
 import { HttpResponse, HttpRequest } from '../interfaces/http-interface'
 
 export class RegisterVehicle {
@@ -7,7 +8,7 @@ export class RegisterVehicle {
       if (httpRequest.body[props] === undefined) {
         return {
           statusCode: 400,
-          body: new Error(`error in the: ${props}`)
+          body: new MissingFormalParameter(`${props}`)
         }
       }
     }
